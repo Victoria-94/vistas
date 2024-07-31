b8e2a207cdd65959e8b4c62b4230452d8680ef1b
Victoria-94 <146044863+Victoria-94@users.noreply.github.com>
2024-07-30 23:24:58 -0500

    Initial commit

---
+++ portada.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.getElementById('cta-button');
+
+    ctaButton.addEventListener('click', function() {
+        alert('¡Gracias por tu interés en el ciclismo! Visita nuestra sección de noticias para más información.');
+    });
+});
